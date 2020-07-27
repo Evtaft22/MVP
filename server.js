788b19b5dee317f1fa61f195576f3301a5e26e34
Evan Taft <evtaft22@gmail.com>
2020-07-27 12:40:11 -0500

    pkg.json, server listens, database connects

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.listen(PORT, err => {
+	if (err) {
+		console.error(err, 'Could not hear PORT');
+	} else {
+		console.log(`Listening on PORT:${PORT}`);
+	}
+});
+
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'mvp'
+});
+
+db.connect(err => {
+	if (err) {
+		console.error(err, 'database not connecting');
+	} else {
+		console.log('Database is connected');
+	}
+});
+
+module.exports.app = app;
