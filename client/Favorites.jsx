b7afdad2162864eca6e876d49e07bfd67997f505
Evan Taft <evtaft22@gmail.com>
2020-07-28 17:18:46 -0500

    Favorites list component set up

---
+++ client/Favorites.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Movie from './Movie.jsx'
+
+const Favorites = ({ favorites, home, remove }) => (
+  <div>
+    <button id='home' type='button' onClick={() => home()} >Back To Search</button>
+    <h1>My Favorites</h1>
+    <div>
+      {favorites.map(({ 
+        id,
+        title,
+        director,
+        actors,
+        plot,
+        poster }) => (
+          <Movie key={id} poster={poster} title={title} director={director} actors={actors} plot={plot} remove={remove} />
+      ))}
+    </div>
+  </div>
+);
+
+export default Favorites;
