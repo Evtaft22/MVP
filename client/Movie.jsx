882fa37f230d85d187728a288b4c4519947ebad4
Evan Taft <evtaft22@gmail.com>
2020-07-28 17:19:45 -0500

    Movie comp for organized favs and remove from db

---
+++ client/Movie.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Movie = ({
+  id,
+  title,
+  director,
+  actors,
+  plot,
+  poster,
+  remove,
+}) => (
+  <div id="fav">
+    <div onClick={() => remove()}>
+      <img src={poster} />
+    </div>
+    <div>{title}</div>
+    <div>Dircted By: {director}</div>
+    <div>Starring: {actors}</div>
+    <div>Plot: {plot}</div>
+  </div>
+);
+
+export default Movie;
