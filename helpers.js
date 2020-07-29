45ec0368890af17c31ceacb8c4bdceb7eb006096
Evan Taft <evtaft22@gmail.com>
2020-07-28 19:22:29 -0500

    tried to switch to callbacks built middleware

--- helpers.js
+++ helpers.js
@@ -5,34 +5,55 @@ const { db } = require('./server');
 // if not it will add it to the db
 // takes in mvoie data from the API
 // organizes specific data and adds it to the favorites table in the DB
-const postFavorite = ({
-  Title,
-  Year,
-  Rated,
-  Released,
-  Runtime,
-  Genre,
-  Director,
-  Actors,
-  Plot,
-  Poster,
-  imdbRating,
-}) => {
+const postFavorite = (movieObj) => {
+  const {
+    Title,
+    Year,
+    Rated,
+    Released,
+    Runtime,
+    Genre,
+    Director,
+    Actors,
+    Plot,
+    Poster,
+    imdbRating,
+  } = movieObj;
   const moviesQuery = `INSERT INTO favorites (title, year, rated, released,
                     runtime, genre, director, actors, plot, poster, rating)
                     VALUES (${Title}, ${Year}, ${Rated}, ${Released},
                     ${Runtime}, ${Genre}, ${Director}, ${Actors},
                     ${Plot}, ${Poster}, ${imdbRating});`;
   return new Promise((resolve, reject) => {
-    db.query(moviesQuery, err => {
+    db.query(moviesQuery, movieObj, err => {
       if (err) {
         reject(console.error(err, "Couldn't add movie data to DB."));
       } else {
-        resolve('Movie data was added to DB successfully!');
+        resolve(console.log('Movie data was added to DB successfully!'));
       }
     });
   });
 };
+
+// const postFavorite = (movieObj, callback) => {
+//   const moviesQuery = `INSERT INTO favorites (title, year, rated, released,
+//                     runtime, genre, director, actors, plot, poster, rating)
+//                     VALUES (${movieObj.Title}, ${movieObj.Year}, ${movieObj.Rated}, ${movieObj.Released},
+//                     ${movieObj.Runtime}, ${movieObj.Genre}, ${movieObj.Director}, ${movieObj.Actors},
+//                     ${movieObj.Plot}, ${movieObj.Poster}, ${movieObj.imdbRating});`;
+//   db.query(moviesQuery, movieObj, (err, data) => {
+//     if (err) {
+//       console.error(err, "Couldn't add movie data to DB.");
+//       callback(err, null);
+//     } else {
+//       console.log('Movie data was added to DB successfully!');
+//       callback(null, data);
+//     }
+//   });
+// };
+
+
+
 
 //  when a user searches for a movie, by title,
 // the title is routed to the post and get routes in movies.js
--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 require('dotenv').config();
-const mysql = require('mysql2');
 const express = require('express');
-const path = require('path');
-
-const { PORT } = process.env;
-
 const app = express();
+const path = require('path');
+const cors = require('cors');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(PORT, err => {
-	if (err) {
-		console.error(err, 'Could not hear PORT');
-	} else {
-		console.log(`Listening on PORT:${PORT}`);
-	}
-});
+const router = express.Router();
+app.use('/server.js', cors(), router);
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, './index.html'));
-});
+const { getMovie } = require('./client/apiHelper')
+const { postFavorite, getFavorites } = require('./helpers');
+const { PORT } = process.env;
 
 const db = mysql.createConnection({
 	host: 'localhost',
@@ -34,4 +30,54 @@ db.connect(err => {
 	}
 });
 
-module.exports.app = app;
+app.listen(PORT, err => {
+	if (err) {
+		console.error(err, 'Could not hear PORT');
+	} else {
+		console.log(`Listening on PORT:${PORT}`);
+	}
+});
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
+
+
+// app.post = ('/postMovies', (req, res) => {
+//   console.log(req.body, 'for rendering movies');
+//   const { title } = req.body;
+//   getMovie(title)
+//   .then(movieData => res.status(201).send(movieData))
+//   .catch(err => res.status(500).send(console.error(err, 'Could not add movie to favorites.')));
+// });
+
+
+// recieves get request from client through the componentDidMount
+// grabs all favorites from database
+// renders them to the page
+app.get('/getFavs', (req, res) => {
+  res.send(console.log('hey'));
+  getFavorites()
+  .then(favsList => res.status(200).send(favsList))
+  .catch(err => res.status(500).send(console.error(err, 'Could not get your favorites.')));
+});
+
+// recieves post from handle favs
+app.post('/postFavs', (req, res) => {
+	console.log(req.body, 'for adding favs'); // should be a title
+	const { title } = req.body;
+	getMovie(title)
+  .then(movieData => {
+		return Promise.resolve(postFavorite(movieData));
+	})
+  .then(res => res.status(201).send(console.log('Movie was added to favorites.')))
+  .catch(err => res.status(500).send(console.error(err, 'Could not add movie to favorites.')));
+});
+
+// delete favorite by id
+app.delete('/delete', (req, res) => {
+	console.log(req.body, 'for delete') // should be an id
+});
+
+module.exports = {
+	app,
+	db
+};
